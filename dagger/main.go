b9dfec8fc97c24a0e3c2ac83e0124ce76ac17a86
Taylor Monacelli <taylormonacelli@gmail.com>
2025-01-01 22:34:26 -0500

    fail

--- dagger/main.go
+++ dagger/main.go
@@ -1,17 +1,3 @@
-// A generated module for Grayidea functions
-//
-// This module has been generated via dagger init and serves as a reference to
-// basic module structure as you get started with Dagger.
-//
-// Two functions have been pre-created. You can modify, delete, or add to them,
-// as needed. They demonstrate usage of arguments and return types using simple
-// echo and grep commands. The functions can be called from the dagger CLI or
-// from one of the SDKs.
-//
-// The first line in this comment block is a short description line and the
-// rest is a long description with more detail on the module's purpose or usage,
-// if appropriate. All modules should have a short description.
-
 package main
 
 import (
@@ -20,14 +6,18 @@ import (
 	"dagger/grayidea/internal/dagger"
 )
 
-type Grayidea struct{}
+type Grayidea struct {
+	Anyprotocol *dagger.Anyprotocol
+}
 
-// Returns a container that echoes whatever string argument is provided
+func (m *Grayidea) Hello(ctx context.Context) (string, error) {
+	return m.Anyprotocol.HelloModule().Stdout(ctx)
+}
+
 func (m *Grayidea) ContainerEcho(stringArg string) *dagger.Container {
 	return dag.Container().From("alpine:latest").WithExec([]string{"echo", stringArg})
 }
 
-// Returns lines that match a pattern in the files of the provided Directory
 func (m *Grayidea) GrepDir(ctx context.Context, directoryArg *dagger.Directory, pattern string) (string, error) {
 	return dag.Container().
 		From("alpine:latest").
